c1f22f861c3a12161a41fae2d7644fe87f6911e2
shou <nsshowdream@gmail.com>
2018-03-05 19:50:01 +0900

    add under_floor() method to calc cut float value

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
+var app = express();
+
+var under_floor = function(value,n){
+  var result = Math.floor(value*Math.pow(10,n))/Math.pow(10,n);
+  return result
+}
 
 app.get("/",function(req,res){
+  var man = Math.random();
+  var women = 1.0 - man;
+  man = under_floor(man,2);
+  women = under_floor(women,2);
   res.json({"foo":"bar"});
 })
 
